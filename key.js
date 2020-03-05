aba9f04a72da583e21ad389cd46a1177cd34cc4d
Taisei Watanabe <yomichi.o.terasu.house@gmail.com>
2020-03-06 02:36:59 +0900

    your_Idとかに変えた

--- key.js
+++ key.js
@@ -1,14 +1,14 @@
 //////keys
 ////line
-//var channel_access_token = 'qn7G7ooyPk14r+ccerwuokY/Eh44dbrCCh18x+sTowsK+FCIEE1wYQcCaZ3+a8+oPtlsgO6GooYWRM0LbLQy5IWajmvbMIioUoGG+Uar91mMNd+EW+q7hDVS29UjbTyBdMtBi1u4YInQO7+2q2B0VwdB04t89/1O/w1cDnyilFU=';
-//var MASTER_USERID = "U2012df34792adf4ce94b2b36d669bd59";
-//var HWID_HOME = "000002b868";
-//var HWID_LAB = "012c669b82";
+//var channel_access_token = 'your_token';
+//var MASTER_USERID = "your_Id";
+//var HWID_HOME = "any_hwid";
+//var HWID_LAB = "any_hwid";
 ////spreadSheet
-//var spreadSheet_Id = "1JqCp_y7eOn6e4QPlGbCXHIPnsrvU0EUE2-_-MFTmNXw";
+//var spreadSheet_Id = "any_spreadSheet_Id";
 ////ニフクラ
-//var application_key = "b472d2ab52ea8c125dd824e2f2da7a3804263d15dd8ae9c2abfab01546e2ca4c";
-//var client_key = "1c6e63f8db4776114481fbc1129ab2cc59f3bf0e2faa1d0eb48e83be32cbf1d6";
+//var application_key = "your_key";
+//var client_key = "your_key";
 //
 //////共通変数定義
 //var CHANNEL_ACCESS_TOKEN;
